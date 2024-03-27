d474e60e0d1d0eaf156c9fa59b7e1d69b55742a9
surya-jat <126251009+surya-jat@users.noreply.github.com>
2024-03-28 02:52:35 +0530

    Update client.js

--- static/js/client.js
+++ static/js/client.js
@@ -4,7 +4,7 @@
 // By adding the js file --> http://localhost:8000/socket.io/socket.io.js
 // We are now connected to the socket server
 
-const socket = io('localhost:5001');
+const socket = io('http://localhost:5001');
 const form = document.getElementById('message-sender');
 const messageInput = document.getElementById('message');
 
